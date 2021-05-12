35233e5233c4257cb1800fca80c4dc62ac0c434e
Frapo <frapo_developer@outlook.com.pe>
2021-05-12 04:58:26 -0500

    v1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Banner from './components/Banner.jsx'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
